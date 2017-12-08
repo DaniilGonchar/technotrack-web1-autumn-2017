a2bf9f76337dbfaccbdfbb9f2150db16502f13a6
DanielGonchar <dannygonchar@gmail.com>
2017-12-08 16:54:10 +0300

    hh

--- project/project/application/wsgi.py
+++ project/project/application/wsgi.py
@@ -17,4 +17,4 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "application.settings")
 
 application = get_wsgi_application()
 
-application = DjangoWhiteNoise(application) # added 
+application = DjangoWhiteNoise(application) # added
